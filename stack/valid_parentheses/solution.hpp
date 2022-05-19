6b033b43c4ec312b92838a495aec9040845fa30b
shanedanielwinand <shanewinand@gmail.com>
2022-05-19 16:14:56 -0700

    finished valid_parens

--- stack/valid_parentheses/solution.hpp
+++ stack/valid_parentheses/solution.hpp
@@ -6,15 +6,16 @@
 
 class Solution {
     private:
-    Stack<char> stack;
 
     public:
     bool isValid(std::string str) {
+        Stack<char> stack;
         for (unsigned int i = 0; i < str.size(); i++) {
             if (str[i] == '(') {
                 stack.add('(');
-            } else if (str[i] == ')') {
-                //if (stack.pop())
+            }  else if (str[i] == ')') {
+                if (stack.getSize() == 0) { return false; }
+                stack.pop();
             }
         }
         return !((bool) stack.getSize());
